b2564b6e335b9a07f9a2768588676092e074837b
Arthur Leywin <110176533+kamatashi@users.noreply.github.com>
2023-04-22 13:11:16 -0300

    feat: função indiceDoMenor

--- ordenação/menorPMaior.py
+++ ordenação/menorPMaior.py
@@ -1,7 +1,22 @@
 def ordenador(lista):
-    for i in range(len(lista)):
-        j = indiceDoMenor(i, a)
+    for i in range(len(lista)-1):
+        j = indiceDoMenor(i, lista)
+        lista[i], lista[j] = lista[j], lista[i]
+    
+    return lista
+
 
 
 def indiceDoMenor(i, lista):
-    print('ok')
+    # Usamos um apoio de indíce para guardar as váriaveis que são menores
+    apoio = i
+
+    for k in range(apoio+1, len(lista)):
+        if lista[apoio] > lista[i]:
+            apoio = k
+        
+    return apoio
+
+
+lista = [1,4,57,8,7,4,2,133,1,3]
+print(ordenador(lista))
