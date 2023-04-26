61584e37631ed4e3fe51196f8214cd6d5432527b
Arthur Leywin <110176533+kamatashi@users.noreply.github.com>
2023-04-25 22:54:00 -0300

    funções recursivas
    Tentando usar recurcividade para chegar elementos dentro de uma matriz independente da dimesnão

--- matrizes/matrizProcess.py
+++ matrizes/matrizProcess.py
@@ -1,9 +1,12 @@
-def matrizProcess(matriz):
-  acc = len(matriz)
-  while acc > 0:
-    acc -= 1
+def matrizProcess(matriz, acc=0):
+  if len(matriz) == 0:
+    return matriz
+  else:
+    acc += 1
+    print('ok')
     for elem in matriz:
       print(elem)
+    matrizProcess(matriz[acc], acc)
 
 
 matriz = [[4, 3, 2, 5, 6, 3, 5], [3, 5, 3, 2]]
