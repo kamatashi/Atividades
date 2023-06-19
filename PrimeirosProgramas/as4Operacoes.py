c1cae72f3c26c578568c6a099ee9f9d923cd9d93
Arthur Leywin <110176533+kamatashi@users.noreply.github.com>
2023-06-19 20:28:24 -0300

    Create as4Operacoes.py

---
+++ PrimeirosProgramas/as4Operacoes.py
@@ -0,0 +1,9 @@
+#Está indicando erro na formatação
+
+num1 = float(input())
+num2 = float(input())
+
+print('%.2f' % num1 + num2)
+print('%.2f' % num1 - num2)
+print('%.2f' % num1 * num2)
+print('%.2f' % num1 / num2)
