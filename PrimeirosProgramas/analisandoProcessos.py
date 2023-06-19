74cc9a72656218843df9add9bb426c936c6322f5
Arthur Leywin <110176533+kamatashi@users.noreply.github.com>
2023-06-19 20:19:59 -0300

    Correção analisandoProcessos.py

--- PrimeirosProgramas/analisandoProcessos.py
+++ PrimeirosProgramas/analisandoProcessos.py
@@ -1,2 +1,9 @@
 # Betina trabalha em um escritório de advocacia e todos os dias analisa vários processos. Sabendo que ela cumpre um expediente diário de 8h, escreva um programa que receba como entrada quantos minutos ela leva para analisar cada processo, e exiba o total de processos revisados em um dia de trabalho. (Dica: Uma hora tem 60 minutos)
-tempoProcessos = int(input()) / 60
+# Essa resposta não foi pensada por mim. Analise como funciona a lógica!
+tempoProcessosMin = int(input())
+
+expediente = 8
+
+processosDia = (expediente * 60) // tempoProcessosMin
+
+print(processosDia)
