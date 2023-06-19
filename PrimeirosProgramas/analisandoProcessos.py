9de46c4d144f641fb42ff6c4cd2572df0b439f7c
Arthur Leywin <110176533+kamatashi@users.noreply.github.com>
2023-06-19 20:15:33 -0300

    Update analisandoProcessos.py

--- PrimeirosProgramas/analisandoProcessos.py
+++ PrimeirosProgramas/analisandoProcessos.py
@@ -1,1 +1,2 @@
+# Betina trabalha em um escritório de advocacia e todos os dias analisa vários processos. Sabendo que ela cumpre um expediente diário de 8h, escreva um programa que receba como entrada quantos minutos ela leva para analisar cada processo, e exiba o total de processos revisados em um dia de trabalho. (Dica: Uma hora tem 60 minutos)
 tempoProcessos = int(input()) / 60
