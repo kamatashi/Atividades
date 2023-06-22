f4f683ed4983db72e4d111e9eda49c9c96058f49
Arthur Leywin <110176533+kamatashi@users.noreply.github.com>
2023-06-22 18:05:47 -0300

    Create volumeAreaCilindro.py

---
+++ Expressoes/volumeAreaCilindro.py
@@ -0,0 +1,12 @@
+// Ainda não submetido
+
+pi = 3.14
+
+altura = float(input())
+raio = float(input())
+
+area = 2 * pi * raio * altura
+superficie = altura * (2*pi*raio) + 2*pi*raio**2
+
+print('%.2f' % area)
+print('%.2f' % superficie)
