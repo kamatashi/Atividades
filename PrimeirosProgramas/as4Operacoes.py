15f9567eec9377299a40dc7718a887fc4213295d
Arthur Leywin <110176533+kamatashi@users.noreply.github.com>
2023-06-22 16:31:35 -0300

    fix/bug: erro de formatação
    Isolei a operação dentro do print

--- PrimeirosProgramas/as4Operacoes.py
+++ PrimeirosProgramas/as4Operacoes.py
@@ -1,9 +1,7 @@
-#Está indicando erro na formatação
-
 num1 = float(input())
 num2 = float(input())
 
-print('%.2f' % num1 + num2)
-print('%.2f' % num1 - num2)
-print('%.2f' % num1 * num2)
-print('%.2f' % num1 / num2)
+print('%.2f' % (num1 + num2))
+print('%.2f' % (num1 - num2))
+print('%.2f' % (num1 * num2))
+print('%.2f' % (num1 / num2))
