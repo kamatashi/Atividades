5b48e3edbae4eacdfcee0746b8496e28024c4d02
Dayvid Santana <110176533+kamatashi@users.noreply.github.com>
2026-01-31 20:18:23 -0400

    Create Calculadora.java

---
+++ java/Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora{
+  public static int soma (int a, int b){
+    return a + b;
+  }
+  public static int subtração (int a, intb){
+    return a - b;
+  }
+  public static int multiplicacao (int a, int b){
+    return a * b;
+  }
+  public static int divisao (int a, int b){
+    return a /b
+  }
+}
