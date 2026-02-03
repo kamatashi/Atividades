21fba5e368eda9f10be97e8e41a8a12e75697944
Dayvid Santana <110176533+kamatashi@users.noreply.github.com>
2026-02-02 20:39:47 -0400

    Create TwoSum.c

---
+++ C/TwoSum.c
@@ -0,0 +1,18 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* ok = malloc(2 * sizeof(int));
+    for (int i = 0; i < numsSize; i++){
+        for (int j = i + 1; j < numsSize; j++){
+            if (nums[i] + nums[j] == target){
+                ok[0] = i;
+                ok[1] = j;
+            }
+        }
+    }
+
+    *returnSize = 2;
+
+    return ok;
+}
